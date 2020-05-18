d2806cf616824310c8e60ba961991dbf9e6b2683
dani16595 <dani_16_5_95@hotmail.com>
2020-05-18 13:01:51 +0200

    Bot prueba

---
+++ Bot.py
@@ -0,0 +1,39 @@
+import telegram
+from twisted.internet import task, reactor
+import logging
+
+# Activar logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+TOKEN = "1155082026:AAHBYlC2LFfNP7s_iLIMj6014Qnz-T3K76M"
+CHAT_ID = "@pruebasdanimunoz"
+
+
+timeout = 5.0 # Sixty seconds
+
+# Definimos algunas funciones para los comandos. Estos generalmente toman los dos argumentos update y context
+def start():
+    """Envia un mensaje cuando se emita el comando /start."""
+    global TOKEN
+    global CHAT_ID
+    bot = telegram.Bot(token=TOKEN)
+    bot.sendMessage(chat_id = CHAT_ID, text ="HOLAAAA")
+
+def do_something():
+    print("Doing stuff...")
+    start()
+
+
+
+def main():
+    """Inicio del Bot"""
+    l = task.LoopingCall(do_something)
+    l.start(timeout) # call every sixty seconds
+
+    reactor.run()
+
+
+if __name__ == '__main__':
+    main()
